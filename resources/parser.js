92a9fec9bc05fbe1d8bb5cb5d7ed39bf2441869e
Alexander Morozov <sarmirim@gmail.com>
2020-06-26 22:06:41 +0300

    request() ==> axios()

--- resources/parser.js
+++ resources/parser.js
@@ -1,39 +1,29 @@
-import request from 'request'
 import {default as dataFromParser} from './dataParser.js'
+import axios from'axios'
 
-const link = '';
-const key = '.json?limit=5&raw_json=1';
+const key = '.json?limit=25&raw_json=1';
 
 function parse(linkToParse){
     return new Promise(function(resolve, reject){
-        request(linkToParse + key,
-            function (error, response, body) {
-            resolve(body);
-            reject(new Error('Error'));
-        });
+        axios.get(linkToParse + key)
+        .then(res => {
+            resolve(res.data)
+            reject(new Error('Error'))  
+        })
     });
 }
 
 let jsonData;
-let arrayData = [];
 
 export default async function f(redditLink){
     console.log(redditLink);
-
     let arr = [];
-    jsonData = await parse(redditLink).then(function(val) {
+    jsonData = await parse(redditLink).then((val) => {
         return val;
     }).catch(function(err) {
-        console.err(err);
+        console.error(err);
         return "ERROR";
     });
-    arr = dataFromParser(redditLink, jsonData);
-
+    arr = dataFromParser(redditLink, JSON.stringify(jsonData));
     return arr;
 }
-
-/////Example
-/*f().then((massiv)=>{
-        arrayData = massiv
-        console.table(arrayData)
-    });*/
