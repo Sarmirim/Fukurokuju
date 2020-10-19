38b6ecc1663f9a9014c52a30e208793dcab93522
Alexander Morozov <sarmirim@gmail.com>
2020-10-19 16:04:15 +0300

    delete garbage

--- resources/parser.js
+++ resources/parser.js
@@ -1,29 +1,23 @@
 import {default as dataFromParser} from './dataParser.js'
 import axios from'axios'
 
-const key = '.json?limit=25&raw_json=1';
+const key = '.json?raw_json=1';
 
-function parse(linkToParse){
+function parse(link){
     return new Promise(function(resolve, reject){
-        axios.get(linkToParse + key)
-        .then(res => {
+        axios.get(link + key).then(res => {
             resolve(res.data)
             reject(new Error('Error'))  
         })
     });
 }
 
-let jsonData;
-
-export default async function f(redditLink){
-    console.log(redditLink);
-    let arr = [];
-    jsonData = await parse(redditLink).then((val) => {
-        return val;
+export default async function Parser(link){
+    const jsonData = await parse(link).then((data) => {
+        return data;
     }).catch(function(err) {
         console.error(err);
         return "ERROR";
-    });
-    arr = dataFromParser(redditLink, JSON.stringify(jsonData));
-    return arr;
+    }); 
+    return dataFromParser(link, JSON.stringify(jsonData));
 }
